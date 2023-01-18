53478098b2eca9edbce05587aee3f0319f4be640
Rakshith R Salian <rrsalian16@gmail.com>
2023-01-18 21:06:49 +0400

    feat: add env config

--- App.tsx
+++ App.tsx
@@ -1,70 +1,27 @@
 import React from 'react';
-import {
-    SafeAreaView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import DeviceInfo from 'react-native-device-info';
 import {useForceUpdateApp} from '@Isiri/hooks';
+import Config from 'react-native-config';
 
 const App = () => {
-    const isDarkMode = useColorScheme() === 'dark';
-
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
-
     const version = DeviceInfo.getVersion();
     const buildNumber = DeviceInfo.getBuildNumber();
 
     const {forceUpdateVersion, isForceUpdateEnabled} = useForceUpdateApp();
 
     return (
-        <SafeAreaView style={backgroundStyle}>
-            <StatusBar
-                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-                backgroundColor={backgroundStyle.backgroundColor}
-            />
-
-            <View
-                style={{
-                    backgroundColor: isDarkMode ? Colors.black : Colors.white,
-                }}>
-                <Text style={styles.highlight}>Version :{version}</Text>
-                <Text style={styles.highlight}>
-                    forceUpdateVersion :{forceUpdateVersion}
-                </Text>
-                <Text style={styles.highlight}>
-                    isForceUpdateEnabled :{isForceUpdateEnabled + ''}
-                </Text>
-                <Text style={styles.highlight}>BuildNumber :{buildNumber}</Text>
+        <SafeAreaView>
+            <View>
+                <Text>Version :{version}</Text>
+                <Text>forceUpdateVersion :{forceUpdateVersion}</Text>
+                <Text>isForceUpdateEnabled :{isForceUpdateEnabled + ''}</Text>
+                <Text>BuildNumber :{buildNumber}</Text>
+                <Text>BuildNumber :{JSON.stringify(Config)}</Text>
             </View>
         </SafeAreaView>
     );
 };
-
-const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-    },
-    highlight: {
-        fontWeight: '700',
-    },
-});
 
 export default App;
